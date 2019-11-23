92e277a5002f6aa6fb3ce6384b3f5da489a7b1d1
Fernando <fernandochahua1@gmail.com>
2019-11-23 14:38:58 -0500

    arreglos configuracion

--- src/main/java/com/nutritech/nu34life/ServiceDiscoveryApplication.java
+++ src/main/java/com/nutritech/nu34life/ServiceDiscoveryApplication.java
@@ -2,6 +2,8 @@ package com.nutritech.nu34life;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
